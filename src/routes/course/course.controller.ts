5744436bd489d2b9b85b5810c23560be361b8601
Fynn <fynn@core-dev.de>
2022-10-14 11:59:56 +0000

    add base crud controller

---
+++ src/routes/course/course.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+
+import Course from '../../models/course.model';
+
+
+export default class CourseController {
+    public static async getAllCourses(req: Request, res: Response, next: NextFunction) {
+
+    }
+}
