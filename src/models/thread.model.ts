3f2df920119cc755d5730514f70c2283accb5a02
lumaghg <rotkehlchen567@gmail.com>
2022-10-14 17:13:51 +0200

    added thread routes

--- src/models/thread.model.ts
+++ src/models/thread.model.ts
@@ -3,10 +3,11 @@ import { TThreadDocument, TThreadModel } from '../types/thread.types';
 
 
 const schema = new Schema<TThreadDocument>({
-    course: { type: Schema.Types.ObjectId, ref: 'courses', required: true },
-    createdBy: { type: Schema.Types.ObjectId, ref: 'users', required: true, immutable: true },
+    course: { type: Schema.Types.ObjectId, ref: 'Course', required: true, immutable: true },
+    createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true, immutable: true },
     title: { type: String, required: true },
-    description: { type: String, required: false }
+    description: { type: String, required: false },
+    deleted: { type: Boolean, default: false }
 },
     {
         toObject: {
