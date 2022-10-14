f3a55cef836a055cc15e169fe768128f0f127ccf
lumaghg <rotkehlchen567@gmail.com>
2022-10-14 11:17:57 +0200

    removed route protection of ping

--- src/routes/ping/ping.router.ts
+++ src/routes/ping/ping.router.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import authMiddleware from "../../middleware/auth.middleware";
-import { UserRoles } from "../../types/user.types";
 import PingController from "./ping.controller";
 
 const PingRouter = Router();
 
 //Ping Routes
-PingRouter.get("/",authMiddleware([UserRoles.Student]) ,PingController.ping);
+PingRouter.get("/" ,PingController.ping);
 
 export default PingRouter
