c1aa8d674d74e67337a64e008426fb6fc274422c
Fynn <fynn@core-dev.de>
2022-10-18 12:25:52 +0000

    added Survey as json schema

---
+++ src/types/survey.types.ts
@@ -0,0 +1,22 @@
+import { Document, Model } from "mongoose";
+import { Schema as JSONSchema } from 'ajv';
+
+import { TUserDocument } from './user.types';
+import { TCourseDocument } from './course.types';
+
+export type TSurvey = {
+    title: string
+    description: string | null
+    users: TUserDocument['_id'][]
+    course?: TCourseDocument['_id']
+    answerType: JSONSchema
+    answers: {
+        user: TUserDocument['_id']
+        answer: any
+    }[]
+    deleted: boolean
+}
+
+export interface TSurveyDocument extends TSurvey, Document {}
+
+export interface TSurveyModel extends Model<TSurveyDocument>{}
