06c2bd7b67fc3cdf52f94320e25e5de8281ed958
lumaghg <rotkehlchen567@gmail.com>
2022-10-23 11:25:44 +0200

    fix build

--- src/routes/user/user.router.ts
+++ src/routes/user/user.router.ts
@@ -8,7 +8,7 @@ const UserRouter = Router();
 //User Routes /user
 UserRouter.get("/", authMiddleware([UserRoles.Admin]), UserController.getUserOdatafy);
 UserRouter.get("/my", authMiddleware([UserRoles.Student, UserRoles.Lecturer, UserRoles.Admin]), UserController.getLoggedInUser);
-UserRouter.put("/my", authMiddleware([UserRoles.Student, UserRoles.Lecturer, UserRoles.Admin), UserController.updateLoggedInUser);
+UserRouter.put("/my", authMiddleware([UserRoles.Student, UserRoles.Lecturer, UserRoles.Admin]), UserController.updateLoggedInUser);
 UserRouter.delete("/:id", UserController.deleteUserById);
 
 export default UserRouter
